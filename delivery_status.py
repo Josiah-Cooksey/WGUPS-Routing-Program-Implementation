94fec8c21256966343369c51b7ead6bcc8726f3b
Josiah Cooksey <jcsoftwaretests@gmail.com>
2025-08-22 14:48:06 -0500

    fixes typo

--- delivery_status.py
+++ delivery_status.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 class DeliveryStatus(Enum):
-    # refers to a package not being availabe for delivery at the hub yet
+    # refers to a package not being available for delivery at the hub yet
     DELAYED = 1
     # awaiting delivery
     AT_HUB = 2
