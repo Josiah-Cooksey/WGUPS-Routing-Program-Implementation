3ba880d0b2f054b580be14fefd25637821dd9753
Josiah Cooksey <jcsoftwaretests@gmail.com>
2025-08-23 15:21:38 -0500

    renames before_hash field to "label" for clarity

--- dart_node.py
+++ dart_node.py
@@ -1,17 +1,17 @@
 # can represent a start point and a distance to the next, or and end point and the distance to reach it from your current position
 # depends on context
 class DartNode():
-    def __init__(self, before_hash=None, distance: float=None, key=None):
-        self.before_hash = before_hash
+    def __init__(self, label=None, distance: float=None, key=None):
+        self.label = label
         self.distance = distance
         self.self_hash = None
         if key == None:
-            self.key = self.before_hash
+            self.key = self.label
         else:
             self.key = key
 
     def __repr__(self):
-        return f"node: {self.before_hash}; distance: {self.distance}"
+        return f"node: {self.label}; distance: {self.distance}"
 
     def __str__(self):
-        return self.before_hash
+        return self.label
