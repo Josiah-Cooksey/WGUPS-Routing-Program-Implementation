f55ef651aa371ba74354170ccc12bf355cf35ac2
Josiah Cooksey <jcsoftwaretests@gmail.com>
2025-08-23 17:21:47 -0500

    removes unused "key" field/argument

--- dart_node.py
+++ dart_node.py
@@ -1,13 +1,9 @@
 # can represent a start point and a distance to the next, or and end point and the distance to reach it from your current position
 # depends on context
 class DartNode():
-    def __init__(self, label=None, distance: float=None, key=None):
+    def __init__(self, label=None, distance: float=None):
         self.label = label
         self.distance = distance
-        if key == None:
-            self.key = self.label
-        else:
-            self.key = key
 
     def __repr__(self):
         return f"node: {self.label}; distance: {self.distance}"
