1382258becd2456db2794198ab0024fe02cd2148
Josiah Cooksey <jcsoftwaretests@gmail.com>
2025-08-23 20:05:42 -0500

    fixes mileage accrual whilst trucks wait to be loaded removes unused fields and old TODO

--- truck.py
+++ truck.py
@@ -6,7 +6,7 @@ from mail_item import MailItem
 from delivery_status import DeliveryStatus
 
 class Truck():
-    def __init__(self, id, driver: DeliveryDriver=None, package_capacity: int=16, average_speed: int=18, mileage: int=0):
+    def __init__(self, id, driver: DeliveryDriver=None, package_capacity: int=16, average_speed: int=18):
         self.id = id
         self.package_capacity = package_capacity
         self.average_speed_mph = average_speed
@@ -22,7 +22,7 @@ class Truck():
         self.last_address_departure_time = -1
         self.visited_address_log: List[DartNode] = []
 
-        self.start_time = None
+        self._last_reported_mileage = 0
 
     # should be called whenever the truck has reached a node; the edge "points" to the next node not yet in the list
     def update_status(self, address:DartNode, time:int = 0):
@@ -31,33 +31,37 @@ class Truck():
     # assumes that it's the same day
     def get_current_mileage(self, current_time: int):
         mileage = 0
+        prior_node_distance = 0
+        prior_log_time = 0
         for i in range(len(self.visited_address_log)):
             log_time, log_node = self.visited_address_log[i]
-            if current_time > log_time:
-                # because the log is in chronological order, we only need to sum distances from nodes logged at a time <= the current time
-                mileage += log_node.distance
+            if current_time >= log_time:
+                # because the log is in chronological order, we need to sum distances from nodes logged at a time <= the current time
+                mileage += prior_node_distance
             else:
-                # plus the distance of the final node divided by the distance we've theoretically travelled since we've been there
-                # (the end nodes of a route are always at the HUB and always have a distance of 0,
-                # so this should fix mileage accrual whilst trucks wait at the HUB for packages to become eligible for loading)
-                time_on_that_node = (log_time - current_time)
-                if self.average_speed_mpm != 0 and time_on_that_node != 0:
-                    mileage += (log_node.distance) / (self.average_speed_mpm * time_on_that_node)
+                # plus the distance that we've theoretically travelled since we've been there
+                time_on_last_node = (current_time - prior_log_time)
+                estimated_distance = (self.average_speed_mpm * time_on_last_node)
+                # sometimes when the trucks are waiting to be loaded, the time they wait * their average speed is a longer distance
+                # than what it took to reach where they are
+                # this should fix mileage accrual whilst trucks wait at the HUB for packages to become eligible for loading
+                if estimated_distance < prior_node_distance and time_on_last_node > 0:
+                    mileage += estimated_distance
                 break
+            prior_node_distance = log_node.distance
+            prior_log_time = log_time
         
+        self._last_reported_mileage = mileage
         return mileage
     
     # only needs to be called once per route
     # the truck deletes its route (by setting it to None) after delivering all packages 
     def drive_route(self, start_time: int):
-        self.start_time = start_time
-        
         total_route_distance = 0
         time_when_at_next = start_time
         for node in self.route:
             self.update_status(node, time_when_at_next)
 
-            # TODO: MARK PACKAGES at node.address AS DELIVERED AND REMOVE THEM FROM self.packages
             packages_for_this_address = self.packages.lookup_all(node.label)
             for package in packages_for_this_address:
                 self.deliver(package, time_when_at_next)
@@ -66,7 +70,7 @@ class Truck():
             time_when_at_next += (node.distance / self.average_speed_mpm)
 
         self.route = None
-        self.route_completion_timestamp_minutes = self.start_time + (total_route_distance / self.average_speed_mpm)
+        self.route_completion_timestamp_minutes = start_time + (total_route_distance / self.average_speed_mpm)
 
     def calculate_route_round_trip_distance(self):
         for node in self.route:
