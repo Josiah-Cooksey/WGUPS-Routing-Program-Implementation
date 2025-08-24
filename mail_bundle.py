5633e0bec70c4b5b89375f5dcd54cfd328e90441
Josiah Cooksey <jcsoftwaretests@gmail.com>
2025-08-23 23:35:52 -0500

    implements append() and pop() for convenience

--- mail_bundle.py
+++ mail_bundle.py
@@ -5,9 +5,16 @@ class MailBundle():
     def __init__(self):
         self.packages = []
         self.required_truck = None
+        self.bundled_by = None
 
     def __iter__(self):
         return (p for p in self.packages)
 
     def __len__(self):
         return len(self.packages)
+    
+    def append(self, package):
+        self.packages.append(package)
+    
+    def pop(self):
+        return self.packages.pop(-1)
