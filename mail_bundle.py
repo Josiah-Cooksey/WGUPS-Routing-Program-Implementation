4c9da99703e2f94a4caaf99e0124a386ead78901
Josiah Cooksey <jcsoftwaretests@gmail.com>
2025-08-23 22:42:44 -0500

    implements __iter__() and __len__() for easy parsing

--- mail_bundle.py
+++ mail_bundle.py
@@ -3,7 +3,11 @@ from typing import Set
 # used to group packages that MUST be delivered together
 class MailBundle():
     def __init__(self):
-        self.required_package_IDs = Set()
         self.packages = []
-        self.truck_restriction = None
-    
+        self.required_truck = None
+
+    def __iter__(self):
+        return (p for p in self.packages)
+
+    def __len__(self):
+        return len(self.packages)
