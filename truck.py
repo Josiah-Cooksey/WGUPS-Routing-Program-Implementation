83e1bf89c898e2b90c26ae8f127335175588828a
Josiah Cooksey <jcsoftwaretests@gmail.com>
2025-08-23 19:05:55 -0500

    updates route completion indication and mileage measurement

--- truck.py
+++ truck.py
@@ -15,15 +15,12 @@ class Truck():
         
         self.packages = HashTable()
 
-        self.done_with_route = True
-        self.minimum_spanning_tree = None
+        self.route_completion_timestamp_minutes = 0
         self.route = None
         self.route_total_distance = 0
         self.current_node_index = 0
         self.last_address_departure_time = -1
         self.visited_address_log: List[DartNode] = []
-        # only updated by move() in blocks; should be equal to the sum of distances in visited_address_log except for the last
-        self.blocked_mileage = 0
 
         self.start_time = None
 
@@ -34,20 +31,25 @@ class Truck():
     # assumes that it's the same day
     def get_current_mileage(self, current_time: int):
         mileage = 0
-        for i in range(len(self.visited_address_log) - 2):
+        for i in range(len(self.visited_address_log)):
             log_time, log_node = self.visited_address_log[i]
             if current_time > log_time:
-                mileage += ((current_time - log_time) * self.average_speed_mpm)
-            else:
                 # because the log is in chronological order, we only need to sum distances from nodes logged at a time <= the current time
+                mileage += log_node.distance
+            else:
+                # plus the distance of the final node divided by the distance we've theoretically travelled since we've been there
+                # (the end nodes of a route are always at the HUB and always have a distance of 0,
+                # so this should fix mileage accrual whilst trucks wait at the HUB for packages to become eligible for loading)
+                time_on_that_node = (log_time - current_time)
+                if self.average_speed_mpm != 0 and time_on_that_node != 0:
+                    mileage += (log_node.distance) / (self.average_speed_mpm * time_on_that_node)
                 break
         
         return mileage
     
     # only needs to be called once per route
-    # the truck sets done_with_route to True after delivering all packages 
+    # the truck deletes its route (by setting it to None) after delivering all packages 
     def drive_route(self, start_time: int):
-        self.done_with_route = False
         self.start_time = start_time
         
         total_route_distance = 0
@@ -63,7 +65,8 @@ class Truck():
             total_route_distance += node.distance
             time_when_at_next += (node.distance / self.average_speed_mpm)
 
-        self.done_with_route = True
+        self.route = None
+        self.route_completion_timestamp_minutes = self.start_time + (total_route_distance / self.average_speed_mpm)
 
     def calculate_route_round_trip_distance(self):
         for node in self.route:
@@ -74,7 +77,7 @@ class Truck():
         if isinstance(package_or_packages, MailItem):
             self.packages.remove_by_item(package_or_packages.address, package_or_packages)
 
-        if isinstance(package_or_packages, List):
+        elif isinstance(package_or_packages, List):
             for package in package_or_packages:
                 self.packages.remove_by_item(package.address, package)
     
@@ -83,7 +86,7 @@ class Truck():
         if isinstance(package_or_packages, MailItem):
             self.packages.insert(package_or_packages.address, package_or_packages)
 
-        if isinstance(package_or_packages, List):
+        elif isinstance(package_or_packages, List):
             for package in package_or_packages:
                 self.packages.insert(package.address, package)
     
@@ -93,10 +96,10 @@ class Truck():
             package_or_packages.update_status(DeliveryStatus.DELIVERED, delivery_time)
             self.unload(package_or_packages)
 
-        if isinstance(package_or_packages, List):
+        elif isinstance(package_or_packages, List):
             for package in package_or_packages:
                 package.update_status(DeliveryStatus.DELIVERED, delivery_time)
                 self.unload(package)
     
-    def can_be_loaded(self):
-        return self.done_with_route and len(self.packages) < self.package_capacity
+    def can_be_loaded(self, time):
+        return self.route == None and len(self.packages) < self.package_capacity and time >= self.route_completion_timestamp_minutes
